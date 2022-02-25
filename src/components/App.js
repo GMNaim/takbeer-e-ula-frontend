82a8d974e11b2d293bee69edf5b5872eb5ee3a2c
GMNaim <gmnaim3336@gmail.com>
2022-02-25 19:33:18 +0600

    add nav bar change chart design

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSalatByDate } from "../api/graph";
 import "../styles/styles.css";
+import Nav from "./Nav";
 
 const waqts = ["Fajr", "Juhr", "Asr", "Magrib", "Isha"];
 
@@ -8,14 +9,15 @@ function App() {
   const { salatByDate, dates } = useSalatByDate();
 
   return (
-    <div className="container">
-      <table className="p-5">
+    <div className="">
+      <Nav></Nav>
+      <table className="p-5 mt-6 mx-auto">
         <thead>
-          <th className="border border-slate-800">No. of Days</th>
+          {/* <th className="border border-slate-800">No. of Days</th> */}
           <th className="border border-slate-800">Date</th>
           <th className="border border-slate-800">Waqt</th>
           {salatByDate?.user_code_list?.map((code) => {
-            return <th className="border">{code}</th>;
+            return <th className="border border-slate-800">{code}</th>;
           })}
         </thead>
         <tbody className="border border-slate-800">
@@ -25,14 +27,14 @@ function App() {
               console.log(data);
               return (
                 <tr className="border border-slate-800">
-                  <td className="p-2 border  border-slate-800 text-center">
+                  {/* <td className="p-2 border  border-slate-800 text-center">
                     {Math.round(
                       (new Date(date).getTime() +
                         1000 * 3600 * 24 -
                         new Date(salatByDate.starting_date).getTime()) /
                         (1000 * 3600 * 24)
                     )}
-                  </td>
+                  </td> */}
                   <td className="p-2">
                     {new Date(date).toLocaleDateString("en-US", {
                       year: "numeric",
